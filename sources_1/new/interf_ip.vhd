fe953653205e623fc09057f5df8c330522c6d15a
sp-ino <spinovale97@gmail.com>
2022-06-25 15:39:33 +0200

    Finish writing interf_ip module

--- sources_1/new/interf_ip.vhd
+++ sources_1/new/interf_ip.vhd
@@ -33,13 +33,37 @@ entity interf_ip is
         i_rst: in std_logic;
         i_data_ready: in std_logic;
         i_data: in std_logic_vector (out_len - 1 downto 0);
-        o_data: in std_logic_vector (out_len - 1 downto 0);
+        o_data_seen: out std_logic;
+        o_data: out std_logic_vector (out_len - 1 downto 0)
     );
 end interf_ip;
 
 
 architecture Behavioral of interf_ip is
+
+    signal count: integer;
+    constant max_count: integer := 5; 
+
 begin
-    
+
+    update_output: process (i_ck, i_rst)
+    begin
+
+        if i_rst = '1' then
+            o_data <= (others => '0');
+            count <= 0;
+        elsif rising_edge(i_ck) then
+            if i_data_ready = '1' and count = 0 then
+                o_data <= i_data;
+                o_data_seen <= '1';
+                count <= count + 1;
+            elsif count > 0 and count < max_count then
+                count <= count + 1;
+            elsif count = max_count then
+                count <= 0;
+            end if;
+        end if;
+        
+    end process update_output;
 
 end Behavioral;
