ff582586821723da265905e1cf054cb99cfe3c2d
sp-ino <spinovale97@gmail.com>
2022-07-02 17:36:54 +0200

    Baudrate set to 38400 by choosing bit_duration. Next change will be to directly choos the baudrate and then bit_duration is computed automatically

--- sources_1/new/uart_pkg.vhd
+++ sources_1/new/uart_pkg.vhd
@@ -5,7 +5,7 @@ use IEEE.std_logic_1164.all;
 package uart_pkg is
 
     constant out_len: integer := 8; -- length of an output word
-    constant bit_duration: integer := 8; -- duration of a data bit in clock cycles
+    constant bit_duration: integer := 2604; -- duration of a data bit in clock cycles
     constant frame_len: integer := 8; -- number of data bits in a frame
     type states is (idle, count_cycles, wait_data_seen);
 
